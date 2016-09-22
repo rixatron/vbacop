bd705caef4e8ff836e9ba4cd8383bb5b96761879
Richard Makepeace <r.p.makepeace@gmail.com>
2016-09-22 10:15:20 +0000

    Added handling for negative numbers.

--- src/test/LexTests.fs
+++ src/test/LexTests.fs
@@ -26,20 +26,32 @@ type LexerTests ()=
         actual |> should equal expected
 
 
-    [<Test>]
-     member x.``Test decimal literal intialisation`` () =
-        let actual = doLex "x = 10" 
-        let expected = [ID("x");EQUALS;INT({ Value=10;NumberType=Decimal});EOF]
-        compare expected actual
-
-     [<Test>]
-     member x.``Test octal literal intialisation`` ([<Values("x = &o11","x = &O11", "x = &11")>]i:string) =
+    [<Test; Sequential>]
+     member x.``Test decimal literal intialisation``
+               (
+                 [<Values("x = 10","x = -10")>]i:string,
+                 [<Values(10,-10)>]n:int
+               ) =
         let actual = doLex i
-        let expected = [ID("x");EQUALS;INT({ Value=9;NumberType=Octal});EOF]
+        let expected = [ID("x");EQUALS;INT({ Value=n;NumberType=Decimal});EOF]
+        compare expected actual
+
+     [<Test; Sequential>]
+     member x.``Test octal literal intialisation`` 
+               (
+                 [<Values("x = &o11","x = &O11", "x = &11", "x = -&11")>]i:string,
+                 [<Values(9,9,9,-9)>]n:int
+               ) =
+        let actual = doLex i
+        let expected = [ID("x");EQUALS;INT({ Value=n;NumberType=Octal});EOF]
         compare expected actual
 
     [<Test>]
-     member x.``Test hex literal intialisation`` () =
+     member x.``Test hex literal intialisation`` 
+               (
+                 [<Values("x = &hFF","x = &hff", "x = &HFF", "x = -&HFF")>]i:string,
+                 [<Values(255,255,255,-255)>]n:int
+               ) =
         let actual = doLex "x = &hFF" 
         let expected = [ID("x");EQUALS;INT({ Value=255;NumberType=Hexadecimal});EOF]
         compare expected actual
