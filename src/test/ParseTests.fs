c3f9bb352edca13d5fdc18c17f41deed8d56af2d
Richard Makepeace <r.p.makepeace@gmail.com>
2016-10-01 09:47:19 +0000

    Improved option handling
    Some option statements did not parse correctly.

--- src/test/ParseTests.fs
+++ src/test/ParseTests.fs
@@ -12,9 +12,27 @@ let doParse s =
 [<TestFixture>]
 type ParserTests ()=
 
-  [<Test>] member x.``Test Single option explcit`` ()=
+  [<Test>] 
+  member x.``Test Single option explcit`` ()=
               doParse "Option Explicit
               " |> should equal {  Statements = [(Option(Explicit))]; SubProcedures=[] }
 
+  [<Test>] 
+  member x.``Test Single option compare text`` ()=
+              doParse "Option Compare Text
+              " |> should equal {  Statements = [(Option(CompareOption(Text)))]; SubProcedures=[] }
 
+  [<Test>] 
+  member x.``Test Single option compare binary`` ()=
+              doParse "Option Compare Binary
+              " |> should equal {  Statements = [(Option(CompareOption(Binary)))]; SubProcedures=[] }
 
+  [<Test>] 
+  member x.``Test Single option base`` ()=
+              doParse "Option Base 2
+              " |> should equal {  Statements = [(Option(Base(2)))]; SubProcedures=[] }
+
+  [<Test>] 
+  member x.``Test Single option private`` ()=
+                  doParse "Option Private Module
+                  " |> should equal {  Statements = [(Option(PrivateModule))]; SubProcedures=[] }
